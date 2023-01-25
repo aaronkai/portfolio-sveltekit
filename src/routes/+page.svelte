6153429afbf3b948606fca424aec79ce558ea8f4
aaron <aaron.kai.hubbard@gmail.com>
2023-01-25 12:02:52 -0500

    corrected video link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,7 +32,7 @@
 				>, a proud
 				<a href="https://www.instagram.com/p/CVJmDN-AFbQwIwD9kF2dv9l8IDtB-NtVdJMdJ40/">dad</a>, and
 				a middling
-				<a href="https://https://youtube.com/shorts/gTgbVU5cQaA"> fingerpicker</a>.
+				<a href="https://youtube.com/shorts/gTgbVU5cQaA?feature=share"> fingerpicker</a>.
 			</h1>
 			<p class="md:text-xl max-w-prose">
 				By day, I'm an application administrator for NOAA's CLASS project. In my free-time, I'm
