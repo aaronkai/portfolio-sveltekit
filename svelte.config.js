0d1fa72d2fb007daada67aa7ac542fb405c6a2b0
aaron <aaron.kai.hubbard@gmail.com>
2023-09-27 09:05:57 -0400

    change adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 import { mdsvex } from 'mdsvex';
 import preprocess from 'svelte-preprocess';
 import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
