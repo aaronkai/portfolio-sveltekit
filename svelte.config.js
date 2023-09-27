7260647b13af7aa9393a04672c1f35677ab01240
aaron <aaron.kai.hubbard@gmail.com>
2023-09-27 10:43:23 -0400

    off the rails

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,9 @@
 import { mdsvex } from 'mdsvex';
 import preprocess from 'svelte-preprocess';
 import mdsvexConfig from './mdsvex.config.js';
-<<<<<<< HEAD
 import adapter from '@sveltejs/adapter-auto';
 // import adapter from '@sveltejs/adapter-cloudflare';
 // import adapter from '@sveltejs/adapter-vercel';
-=======
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-cloudflare';
->>>>>>> dev
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
